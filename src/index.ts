bc2bd8d01d3af022265bb04ad5dd674060233a3a
Eli Saado <eli5saado@gmail.com>
2021-03-08 23:07:45 +0100

    Comply with typescript lint rules

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import debug from 'debug';
 import app from './router';
-var debug = require('debug')('index');
+
+const log = debug('index');
 
 app.listen(3000, () => {
-    debug("Listening on port 3000");
+    log("Listening on port 3000");
 });
