fd28f203b6e75ec3042c4fb52e09aba1894bf134
Eli Saado <eli5saado@gmail.com>
2021-03-14 15:16:43 +0100

    Change rest response

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 
 const router = express.Router();
+
 router.get('/', (req, res) => {
-    res.json("Welcome!");
+    res.json({ok: true, data: {msg: "Welcome to the Liber API!"}});
 });
 
 export default router;
